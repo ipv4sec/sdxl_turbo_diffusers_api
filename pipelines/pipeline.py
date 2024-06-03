3c454c9f0ad6ec387fee8aa58c5134343587fe8c
YuYuai <ipv4sec@gmail.com>
2024-06-03 15:37:12 +0800

    feat: add lora

--- pipelines/pipeline.py
+++ pipelines/pipeline.py
@@ -8,10 +8,11 @@ import numpy as np
 import torch
 from PIL import Image
 from controlnet_aux.pidi import PidiNetDetector
-from diffusers import EulerAncestralDiscreteScheduler, DPMSolverSinglestepScheduler, ControlNetModel, \
+from diffusers import DPMSolverSinglestepScheduler, ControlNetModel, \
     StableDiffusionXLControlNetPipeline, AutoPipelineForText2Image, AutoPipelineForInpainting
-from diffusers import StableDiffusionXLAdapterPipeline, T2IAdapter,AutoencoderKL
+from diffusers import StableDiffusionXLAdapterPipeline, T2IAdapter, AutoencoderKL
 
+import utils.prompt
 from pipelines.stablediffusion_xl_reference_pipeline import StableDiffusionXLReferencePipeline
 
 
@@ -21,6 +22,7 @@ class Pipeline:
     __adapter_sketch_path = ""
     __base_model_path = ""
     __pidi_net_path = ""
+    __lora_path = ""
 
     def __init__(self):
         self.get_model_path_from_env()
@@ -38,16 +40,24 @@ class Pipeline:
         self.__base_model_path = os.environ.get("BaseModelPath", "0x4f1f/TurboVisionXL-v3.2")
         self.__pidi_net_path = os.environ.get("PidiNetPath", "lllyasviel/Annotators")
         self.__adapter_sketch_path = os.environ.get("AdapterSketchPath", "TencentARC/t2i-adapter-sketch-sdxl-1.0")
+        self.__lora_path = os.environ.get("LoraPath", "")
+
+    def load_lora(self, pipeline, lora: str, scale: float):
+        lora_path = "{}/{}.safetensors".format(self.__lora_path, lora)
+        if self.__lora_path.endswith("/"):
+            lora_path = "{}{}.safetensors".format(self.__lora_path, lora)
+        pipeline.load_lora_weights(lora_path)
+        pipeline.fuse_lora(lora_scale=scale)
 
     # load model
     def load_model(self):
         # Base pipeline
         self.__pipeline = AutoPipelineForText2Image.from_pretrained(
-            self.__base_model_path, 
+            self.__base_model_path,
             torch_dtype=torch.float16,
             variant="fp16",
             vae=AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
-            ).to("cuda")
+        ).to("cuda")
         self.__pipeline.scheduler = DPMSolverSinglestepScheduler.from_config(self.__pipeline.scheduler.config)
         # T2I Adapter Sketch pipeline
         # 使用同一个 pipeline.compontent 会有干扰
@@ -55,11 +65,18 @@ class Pipeline:
         self.__pipeline_adapter = StableDiffusionXLAdapterPipeline.from_pretrained(self.__base_model_path,
                                                                                    torch_dtype=torch.float16,
                                                                                    variant="fp16",
-                                                                                   adapter=T2IAdapter.from_pretrained(self.__adapter_sketch_path, torch_dtype=torch.float16,variant="fp16").to('cuda'),
-                                                                                   vae=AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16),
-                                                                                   scheduler=DPMSolverSinglestepScheduler.from_pretrained(self.__base_model_path, subfolder='scheduler')
+                                                                                   adapter=T2IAdapter.from_pretrained(
+                                                                                       self.__adapter_sketch_path,
+                                                                                       torch_dtype=torch.float16,
+                                                                                       variant="fp16").to('cuda'),
+                                                                                   vae=AutoencoderKL.from_pretrained(
+                                                                                       "madebyollin/sdxl-vae-fp16-fix",
+                                                                                       torch_dtype=torch.float16),
+                                                                                   scheduler=DPMSolverSinglestepScheduler.from_pretrained(
+                                                                                       self.__base_model_path,
+                                                                                       subfolder='scheduler')
                                                                                    ).to("cuda")
-        
+
         # # Reference pipeline
         self.__pipeline_reference = StableDiffusionXLReferencePipeline(
             vae=self.__pipeline.vae,
@@ -94,9 +111,16 @@ class Pipeline:
                  num_inference_steps: int = 5, guidance_scale: float = 3) -> List[str]:
         self.__jobs += 1
         try:
-            return self.__pipeline(prompt=prompt, negative_prompt=negative_prompt,
-                                   num_inference_steps=5, guidance_scale=2.5, width=width,
-                                   height=height).images
+            a = utils.prompt.parser(prompt)
+            if a.lora is not None:
+                self.load_lora(self.__pipeline, a.lora.value, a.lora.scale)
+            return self.__pipeline(
+                prompt=a.value,
+                negative_prompt=negative_prompt,
+                num_inference_steps=num_inference_steps,
+                guidance_scale=2.5,
+                width=width,
+                height=height).images
         except Exception as e:
             raise e
         finally:
@@ -111,10 +135,18 @@ class Pipeline:
             input_image_sketch = self.__pidinet(
                 image, detect_resolution=1024, image_resolution=1024, apply_filter=True
             )
-            images = self.__pipeline_adapter(prompt=prompt, negative_prompt=negative_prompt, image=input_image_sketch,
-                                             num_inference_steps=5, guidance_scale=4,
-                                             adapter_conditioning_scale=adapter_conditioning_scale, width=width,
-                                             height=height).images
+            a = utils.prompt.parser(prompt)
+            if a.lora is not None:
+                self.load_lora(self.__pipeline_adapter, a.lora.value, a.lora.scale)
+            images = self.__pipeline_adapter(
+                prompt=a.value,
+                negative_prompt=negative_prompt,
+                image=input_image_sketch,
+                num_inference_steps=num_inference_steps,
+                guidance_scale=guidance_scale,
+                adapter_conditioning_scale=adapter_conditioning_scale,
+                width=width,
+                height=height).images
             images.append(input_image_sketch)
             return images
         except Exception as e:
@@ -123,16 +155,19 @@ class Pipeline:
             self.__jobs -= 1
 
     def text2img_reference(self, prompt: str, image: Image, width: int = 512, height: int = 512,
-                           negative_prompt: str = "", num_inference_steps: int = 5, guidance_scale: float = 0,
+                           negative_prompt: str = "", num_inference_steps: int = 5, guidance_scale: float = 1.5,
                            control_guidance_start: float = 0.6, control_guidance_end: float = 1) -> str:
         self.__jobs += 1
         try:
+            a = utils.prompt.parser(prompt)
+            if a.lora is not None:
+                self.load_lora(self.__pipeline_reference, a.lora.value, a.lora.scale)
             return self.__pipeline_reference(ref_image=image,
-                                             prompt=prompt,
+                                             prompt=a.value,
                                              width=width,
                                              height=height,
-                                             num_inference_steps=5,
-                                             guidance_scale=1.5,
+                                             num_inference_steps=num_inference_steps,
+                                             guidance_scale=guidance_scale,
                                              reference_attn=True,
                                              reference_adain=True).images[0]
         except Exception as e:
@@ -152,14 +187,17 @@ class Pipeline:
             image = np.concatenate([image, image, image], axis=2)
             canny_image = Image.fromarray(image)
             canny_image = canny_image.resize((width, height))
+            a = utils.prompt.parser(prompt)
+            if a.lora is not None:
+                self.load_lora(self.__pipeline_canny, a.lora.value, a.lora.scale)
             return self.__pipeline_canny(
-                prompt,
+                a.value,
                 image=canny_image,
                 width=width,
                 height=height,
-                num_inference_steps=5,
+                num_inference_steps=num_inference_steps,
                 controlnet_conditioning_scale=0.5,
-                guidance_scale=3).images[0]
+                guidance_scale=guidance_scale).images[0]
         except Exception as e:
             raise e
         finally:
